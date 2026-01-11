ab95eed91857fd52eef1c9a621f0237d8b0511f6
YosuaCH <yosuach38@gmail.com>
2026-01-11 17:06:17 +0700

    try to make login page

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,12 +1,12 @@
 function initNavbar() {
   const joinWrapper = document.getElementById("joinWrapper");
-  if (!joinWrapper) return;
 
   window.addEventListener("scroll", () => {
     if (window.scrollY > 200) {
-      joinWrapper.style.width = "160px";
+      joinWrapper.style.width = "130px";
     } else {
       joinWrapper.style.width = "0px";
     }
   });
 }
+document.addEventListener("DOMContentLoaded", initNavbar);
