f0cebfd3f2f993b397be1ffcd0dab1e4890c9237
YosuaCH <yosuach38@gmail.com>
2026-02-18 01:24:38 +0700

    try to chance alert info email or password wrong to UI

--- assets/js/pages/login.js
+++ assets/js/pages/login.js
@@ -1,23 +1,59 @@
 import { login } from "../services/authService.js";
+
 const loginForm = document.getElementById("loginForm");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const emailError = document.getElementById("emailError");
+const passwordError = document.getElementById("passwordError");
+
+const clearError = (input, errorSpan) => {
+  input.classList.remove("ring-2", "ring-red-500", "focus:ring-red-500");
+  errorSpan.classList.add("hidden");
+  errorSpan.innerText = "";
+};
+
+emailInput.addEventListener("input", () => clearError(emailInput, emailError));
+passwordInput.addEventListener("input", () =>
+  clearError(passwordInput, passwordError),
+);
 
 loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  clearError(emailInput, emailError);
+  clearError(passwordInput, passwordError);
 
   try {
-    const res = await login(email, password);
+    const res = await login(emailInput.value, passwordInput.value);
     const data = await res.json();
 
     if (res.ok) {
-      alert(data.message);
+      alert("Login Berhasil!");
       window.location.href = "dashboard.html";
     } else {
-      alert("Gagal: " + data.message);
+      const message = data.message;
+
+      if (message.includes("Email")) {
+        emailInput.classList.add(
+          "ring-2",
+          "ring-red-500",
+          "focus:ring-red-500",
+        );
+        emailError.innerText = message;
+        emailError.classList.remove("hidden");
+      } else if (message.includes("Password")) {
+        passwordInput.classList.add(
+          "ring-2",
+          "ring-red-500",
+          "focus:ring-red-500",
+        );
+        passwordError.innerText = message;
+        passwordError.classList.remove("hidden");
+      } else {
+        alert(message);
+      }
     }
   } catch (err) {
-    alert("Server tidak bisa diakses");
+    alert("Koneksi gagal: " + err);
   }
 });
