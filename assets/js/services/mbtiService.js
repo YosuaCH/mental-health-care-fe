92d5840c54b27aca520a4e6a5764bd3e17358def
YosuaCH <yosuach38@gmail.com>
2026-02-23 22:56:53 +0700

    fix bug on mbtiService.js

--- assets/js/services/mbtiService.js
+++ assets/js/services/mbtiService.js
@@ -3,7 +3,13 @@ const MBTI_API_URL = "http://127.0.0.1:8080/mbti";
 const mbtiService = {
   async getMbtiDetail(code) {
     try {
-      const response = await fetch(`${MBTI_API_URL}/${code}`);
+      const response = await fetch(`${MBTI_API_URL}/${code}`, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
       if (!response.ok) throw new Error("Gagal mengambil data MBTI");
 
       const result = await response.json();
