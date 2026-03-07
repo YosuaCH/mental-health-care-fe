1dccababd164c1b81f29e5446e7dd0f86ba261e2
YosuaCH <yosuach38@gmail.com>
2026-03-08 04:34:00 +0700

    fix bug on api rul mbti_book

--- assets/js/services/mbti_book.js
+++ assets/js/services/mbti_book.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/api";
+const API_URL = "http://127.0.0.1:8080/api";
 export const fetchAllEbooks = async () => {
   try {
     const response = await fetch(`${API_URL}/ebooks`, {
