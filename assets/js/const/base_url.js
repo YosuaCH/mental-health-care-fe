9bae576b302f839979488e7f05c0e71833844b66
YosuaCH <yosuach38@gmail.com>
2026-04-12 20:25:53 +0700

    fix bug responsif on chat.html & make base_url configuration

--- assets/js/const/base_url.js
+++ assets/js/const/base_url.js
@@ -1,2 +1,7 @@
 export const DICEBEAR_BASE_URL = "https://api.dicebear.com/9.x/avataaars/svg";
-export const BACKEND_URL = `http://${window.location.hostname}:8080`;
+
+const hostname = window.location.hostname;
+const isLocal = hostname === '127.0.0.1' || hostname === 'localhost' || hostname.startsWith('192.168.');
+
+export const FRONTEND_URL = isLocal ? `http://${hostname}:5500` : "https://your-production-app.com";
+export const BACKEND_URL = isLocal ? `http://${hostname}:8080` : "https://your-production-api.com";
